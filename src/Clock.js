05833fa4cbab730e13a3b4cbdfe1a27a983e0332
vzteufem <marc.teufel@hama.de>
2022-02-20 19:24:05 +0100

    converted into classes, implemented an example in vanilla-js

--- src/Clock.js
+++ src/Clock.js
@@ -4,102 +4,198 @@ const ClockMode = {
     CONTINOUS: 'continous'
 }
 
-let ClockSettings = () => {
-
-    let radius = 50
-    let lineWidth = 0.02
-    let enableDashes = true
-    let dashSizeSmall = 0.05
-    let dashSizeBig = 0.1
-    let dashOuterDistance = 0.05
-    let enableNumbers = false
-    let numberSize = 0.15
-    let numberFont = 'serif'
-    let numberOuterDistance = 0.25
-    let hourPointerLength = 0.60
-    let minutePointerLength = 0.75
-    let secondPointerLength=  0.7
-    let secondPointerColor= '#1890FF'
-    let secondPointerWidth = 0.01
-    let dotRadius = 0.03
-
-    return {
-        setRadius: (radius) => this.radius = radius,
-        setLineWidth: (lineWidth) => this.lineWidth = lineWidth,
-        setEnableDashes: (enableDashes) => this.enableDashes = enableDashes,
-        setDashSizeSmall: (dashSizeSmall) => this.dashSizeSmall = dashSizeSmall,
-        setDashSizeBig: (dashSizeBig) => this.dashSizeBig = dashSizeBig,
-        setDashOuterDistance: (dashOuterDistance) => this.dashOuterDistance = dashOuterDistance,
-        setEnableNumbers: (enableNumbers) => this.enableNumbers = enableNumbers,
-        setNumberSize: (numberSize) => this.numberSize = numberSize,
-        setNumberFont: (numberFont) => this.numberFont = numberFont,
-        setNumberOuterDistance: (numberOuterDistance) => this.numberOuterDistance = numberOuterDistance,
-        setHourPointerLength: (hourPointerLength) => this.hourPointerLength = hourPointerLength,
-        setMinutePointerLength: (minutePointerLength) => this.minutePointerLength = minutePointerLength,
-        setSecondPointerLength: (secondPointerLength) => this.secondPointerLength = secondPointerLength,
-        setSecondPointerColor: (secondPointerColor) => this.secondPointerColor = secondPointerColor,
-        setSecondPointerWidth: (secondPointerWidth) => this.secondPointerWidth = secondPointerWidth,
-        setDotRadius: (dotRadius) => this.dotRadius,
-        getRadius: () => this.radius,
-        getLineWidth: () => this.lineWidth * this.radius,
-        areDashesEnabled: () => this.enableDashes,
-        getDashSizeSmall: () => this.dashSizeSmall * this.radius,
-        getDashSizeBig: () => this.dashSizeBig * this.radius,
-        getDashOuterDistance: () => this.dashOuterDistance * this.radius,
-        areNumbersEnabled: () => this.enableNumbers,
-        getNumberSize: () => this.numberSize * this.radius,
-        getNumberFont: () => this.numberFont,
-        getNumberOuterDistance: () => this.numberOuterDistance * this.radius,
-        getHourPointerLength: () => this.hourPointerLength * this.radius,
-        getMinutePointerLength: () => this.minutePointerLength * this.radius,
-        getSecondPointerLength: () => this.secondPointerLength * this.radius,
-        getSecondPointerColor: () => this.secondPointerColor,
-        getSecondPointerWidth: () => this.secondPointerWidth  * this.radius,
-        getDotRadius: () => this.dotRadius
+class ClockSettings {
+    constructor() {
+        this.radius = 50
+        this.lineWidth = 0.02
+        this.enableDashes = true
+        this.dashSizeSmall = 0.05
+        this.dashSizeBig = 0.1
+        this.dashOuterDistance = 0.05
+        this.enableNumbers = false
+        this.numberSize = 0.15
+        this.numberFont = 'sans-serif'
+        this.numberOuterDistance = 0.25
+        this.hourPointerLength = 0.60
+        this.minutePointerLength = 0.75
+        this.secondPointerLength=  0.7
+        this.secondPointerColor= '#1890FF'
+        this.secondPointerWidth = 0.01
+        this.dotRadius = 0.03
     }
+
+    setRadius(value) {
+        this.radius = value
+    }
+
+    setLineWidth(value) {
+        this.lineWidth = value
+    }
+
+    setEnableDashes(value) {
+        this.enableDashes = value
+    }
+
+    setDashSizeSmall(value) {
+        this.dashSizeSmall = value
+    }
+
+    setDashSizeBig(value) {
+        this.dashSizeBig = value
+    }
+
+    setDashOuterDistance(value) {
+        this.dashOuterDistance = value
+    }
+
+    setEnableNumbers(value) {
+        this.enableNumbers = value
+    }
+
+    setNumberSize(value) {
+        this.numberSize = value
+    }
+
+    setNumberFont(value) {
+        this.numberFont = value
+    }
+
+    setNumberOuterDistance(value) {
+        this.numberOuterDistance = value
+    }
+
+    setHourPointerLength(value) {
+        this.hourPointerLength = value
+    }
+
+    setMinutePointerLength(value) {
+        this.minutePointerLength = value
+    }
+
+    setSecondPointerLength(value) {
+        this.secondPointerLength = value
+    }
+
+    setSecondPointerColor(value) {
+        this.secondPointerColor = value
+    }
+
+    setSecondPointerWidth(value) {
+        this.secondPointerWidth = value
+    }
+
+    setDotRadius(value) {
+        this.dotRadius = value
+    }
+
+    getRadius() {
+        return this.radius
+    }
+
+    getLineWidth() {
+        return this.lineWidth * this.radius
+    }
+
+    areDashesEnabled() {
+        return this.enableDashes;
+    }
+
+    getDashSizeSmall() {
+        return this.dashSizeSmall * this.radius
+    }
+
+    getDashSizeBig() {
+        return this.dashSizeBig * this.radius;
+    }
+
+    getDashOuterDistance() {
+        return this.dashOuterDistance * this.radius;
+    }
+
+    areNumbersEnabled() {
+        return this.enableNumbers;
+    }
+
+    getNumberSize() {
+        return this.numberSize * this.radius;
+    }
+
+    getNumberFont() {
+        return this.numberFont;
+    }
+
+    getNumberOuterDistance() {
+        return this.numberOuterDistance * this.radius;
+    }
+
+    getHourPointerLength() {
+        return this.hourPointerLength * this.radius;
+    }
+
+    getMinutePointerLength() {
+        return this.minutePointerLength * this.radius;
+    }
+
+    getSecondPointerLength() {
+        return this.secondPointerLength * this.radius;
+    }
+
+    getSecondPointerColor() {
+        return this.secondPointerColor;
+    }
+
+    getSecondPointerWidth() {
+        return this.secondPointerWidth * this.radius;
+    }
+
+    getDotRadius() {
+        return this.dotRadius;
+    }
+
 }
 
-let Clock = (canvas, x, y, mode, settings, timeFunction) => {
+class Clock {
+    constructor(canvas, x, y, mode, settings, timeFunction) {
+        this.canvas = canvas;
+        this.x = x
+        this.y = y
+        this.mode = mode
+        this.settings = settings
+        this.timeFunction = timeFunction
+        this.ctx = this.canvas.getContext('2d')
+    }
 
-    let _canvas = canvas;
-    let _ctx = _canvas.getContext('2d')
-    let _x = x
-    let _y = y
-    let _mode = mode
-    let _settings = settings
-    let _timeFunction = timeFunction
-
-    const _drawDashes = () => {
-        _ctx.save()
-        _ctx.beginPath()
+    drawDashes() {
+        this.ctx.save()
+        this.ctx.beginPath()
         for (let i = 0; i < 60; i++) {
-            const length = (i % 5 === 0) ? _settings.getDashSizeBig() : _settings.getDashSizeSmall()
-            const xFrom = x + (_settings.getRadius() - _settings.getDashOuterDistance()) * Math.sin(i * (Math.PI / 30))
-            const yFrom = y - (_settings.getRadius() -_settings.getDashOuterDistance()) * Math.cos(i * (Math.PI / 30))
-            const xTo = x + (_settings.getRadius() - _settings.getDashOuterDistance() - length) * Math.sin(i * (Math.PI / 30))
-            const yTo = y - (_settings.getRadius() - _settings.getDashOuterDistance() - length) * Math.cos(i * (Math.PI / 30))
-            _ctx.moveTo(xFrom, yFrom)
-            _ctx.lineTo(xTo, yTo)
+            const length = (i % 5 === 0) ? this.settings.getDashSizeBig() : this.settings.getDashSizeSmall()
+            const xFrom = this.x + (this.settings.getRadius() - this.settings.getDashOuterDistance()) * Math.sin(i * (Math.PI / 30))
+            const yFrom = this.y - (this.settings.getRadius() - this.settings.getDashOuterDistance()) * Math.cos(i * (Math.PI / 30))
+            const xTo = this.x + (this.settings.getRadius() - this.settings.getDashOuterDistance() - length) * Math.sin(i * (Math.PI / 30))
+            const yTo = this.y - (this.settings.getRadius() - this.settings.getDashOuterDistance() - length) * Math.cos(i * (Math.PI / 30))
+            this.ctx.moveTo(xFrom, yFrom)
+            this.ctx.lineTo(xTo, yTo)
         }
-        _ctx.stroke()
-        _ctx.restore()
+        this.ctx.stroke()
+        this.ctx.restore()
     }
 
-    const _drawNumbers = () => {
-        _ctx.save()
-        _ctx.textAlign = 'center'
-        _ctx.textBaseline = 'middle'
-        _ctx.font = _settings.getNumberSize() + 'px ' + _settings.getNumberFont()
+    drawNumbers() {
+        this.ctx.save()
+        this.ctx.textAlign = 'center'
+        this.ctx.textBaseline = 'middle'
+        this.ctx.font = this.settings.getNumberSize() + 'px ' + this.settings.getNumberFont()
         for (let i = 1; i < 13; i++) {
-            const numX = _x + (_settings.getRadius() - _settings.getNumberOuterDistance()) * Math.sin(i * (Math.PI / 6))
-            const numY = _y - (_settings.getRadius() - _settings.getNumberOuterDistance()) * Math.cos(i * (Math.PI / 6))
-            _ctx.fillText('' + i, numX, numY)
+            const numX = this.x + (this.settings.getRadius() - this.settings.getNumberOuterDistance()) * Math.sin(i * (Math.PI / 6))
+            const numY = this.y - (this.settings.getRadius() - this.settings.getNumberOuterDistance()) * Math.cos(i * (Math.PI / 6))
+            this.ctx.fillText('' + i, numX, numY)
         }
-        _ctx.restore()
+        this.ctx.restore()
     }
 
-    const _drawPointers = () => {
-        const time = _timeFunction()
+    drawPointers() {
+        const time = this.timeFunction()
         let hourXEnd, hourYEnd,
             minuteXEnd, minuteYEnd,
             secondXEnd, secondYEnd
@@ -107,123 +203,137 @@ let Clock = (canvas, x, y, mode, settings, timeFunction) => {
         const totalMinutes = time.hours * 60 + time.minutes
         const totalSeconds = totalMinutes * 60 + time.seconds
 
-        if (_mode === ClockMode.JUMPING) {
-            hourXEnd = x + _settings.getHourPointerLength() * Math.sin((totalSeconds / 21600) * Math.PI)
-            hourYEnd = y - _settings.getHourPointerLength() * Math.cos((totalSeconds / 21600) * Math.PI)
-            minuteXEnd = x + _settings.getMinutePointerLength() * Math.sin((time.minutes / 30) * Math.PI)
-            minuteYEnd = y - _settings.getMinutePointerLength() * Math.cos((time.minutes / 30) * Math.PI)
-            secondXEnd = x + _settings.getSecondPointerLength() * Math.sin((time.seconds / 30) * Math.PI)
-            secondYEnd = y - _settings.getSecondPointerLength() * Math.cos((time.seconds / 30) * Math.PI)
-        } else if (_mode === ClockMode.CONTINOUS) {
-            hourXEnd = x + _settings.getHourPointerLength() * Math.sin((totalSeconds / 21600) * Math.PI)
-            hourYEnd = y - _settings.getHourPointerLength() * Math.cos((totalSeconds / 21600) * Math.PI)
-            minuteXEnd = x + _settings.getMinutePointerLength() * Math.sin((totalSeconds / 1800) * Math.PI)
-            minuteYEnd = y - _settings.getMinutePointerLength() * Math.cos((totalSeconds / 1800) * Math.PI)
-            secondXEnd = x + _settings.getSecondPointerLength() * Math.sin(((time.seconds * 1000 + time.milliseconds) / 30000) * Math.PI)
-            secondYEnd = y - _settings.getSecondPointerLength() * Math.cos(((time.seconds * 1000 + time.milliseconds) / 30000) * Math.PI)
+        if (this.mode === ClockMode.JUMPING) {
+            hourXEnd = this.x + this.settings.getHourPointerLength() * Math.sin((totalSeconds / 21600) * Math.PI)
+            hourYEnd = this.y - this.settings.getHourPointerLength() * Math.cos((totalSeconds / 21600) * Math.PI)
+            minuteXEnd = this.x + this.settings.getMinutePointerLength() * Math.sin((time.minutes / 30) * Math.PI)
+            minuteYEnd = this.y - this.settings.getMinutePointerLength() * Math.cos((time.minutes / 30) * Math.PI)
+            secondXEnd = this.x + this.settings.getSecondPointerLength() * Math.sin((time.seconds / 30) * Math.PI)
+            secondYEnd = this.y - this.settings.getSecondPointerLength() * Math.cos((time.seconds / 30) * Math.PI)
+        } else if (this.mode === ClockMode.CONTINOUS) {
+            hourXEnd = this.x + this.settings.getHourPointerLength() * Math.sin((totalSeconds / 21600) * Math.PI)
+            hourYEnd = this.y - this.settings.getHourPointerLength() * Math.cos((totalSeconds / 21600) * Math.PI)
+            minuteXEnd = this.x + this.settings.getMinutePointerLength() * Math.sin((totalSeconds / 1800) * Math.PI)
+            minuteYEnd = this.y - this.settings.getMinutePointerLength() * Math.cos((totalSeconds / 1800) * Math.PI)
+            secondXEnd = this.x + this.settings.getSecondPointerLength() * Math.sin(((time.seconds * 1000 + time.milliseconds) / 30000) * Math.PI)
+            secondYEnd = this.y - this.settings.getSecondPointerLength() * Math.cos(((time.seconds * 1000 + time.milliseconds) / 30000) * Math.PI)
         }
 
-        _ctx.save()
+        this.ctx.save()
 
-        _ctx.beginPath()
-        _ctx.moveTo(x, y)
-        _ctx.lineTo(hourXEnd, hourYEnd)
+        this.ctx.beginPath()
+        this.ctx.moveTo(this.x, this.y)
+        this.ctx.lineTo(hourXEnd, hourYEnd)
 
-        _ctx.moveTo(x, y)
-        _ctx.lineTo(minuteXEnd, minuteYEnd)
-        _ctx.stroke()
+        this.ctx.moveTo(this.x, this.y)
+        this.ctx.lineTo(minuteXEnd, minuteYEnd)
+        this.ctx.stroke()
 
-        _ctx.beginPath()
-        _ctx.lineWidth = _settings.getSecondPointerWidth()
-        _ctx.strokeStyle = _settings.getSecondPointerColor()
-        _ctx.moveTo(x, y)
-        _ctx.lineTo(secondXEnd, secondYEnd)
-        _ctx.stroke()
+        this.ctx.beginPath()
+        this.ctx.lineWidth = this.settings.getSecondPointerWidth()
+        this.ctx.strokeStyle = this.settings.getSecondPointerColor()
+        this.ctx.moveTo(this.x, this.y)
+        this.ctx.lineTo(secondXEnd, secondYEnd)
+        this.ctx.stroke()
 
-        _ctx.restore()
+        this.ctx.restore()
     }
 
-    const _drawDot = () => {
-        _ctx.beginPath()
-        _ctx.arc(x, y, _settings.getDotRadius(), 0, 2 * Math.PI)
-        _ctx.fill()
+    drawDot() {
+        this.ctx.beginPath()
+        this.ctx.arc(this.x, this.y, this.settings.getDotRadius(), 0, 2 * Math.PI)
+        this.ctx.fill()
     }
 
-    const _drawArc = () => {
-        _ctx.beginPath()
-        _ctx.arc(x, y, _settings.getDotRadius(), 0, 2 * Math.PI)
-        _ctx.stroke()
+    drawArc = () => {
+        this.ctx.beginPath()
+        this.ctx.arc(this.x, this.y, this.settings.getDotRadius(), 0, 2 * Math.PI)
+        this.ctx.stroke()
     }
 
-    return {
-        getSettings: () => _settings,
-        setX: (x) => _x = x,
-        setY: (y) => _y = y,
-        clearCanvas: () => {
-            _ctx.clearRect(0, 0, canvas.width, canvas.height)
-        },
-        drawClock: () => {
-            _ctx.save()
-            _ctx.lineWidth = _settings.getLineWidth()
-            if (_settings.areDashesEnabled()) _drawDashes()
-            if (_settings.areNumbersEnabled()) _drawNumbers()
-            _drawPointers()
-            _drawDot()
-            _drawArc()
-            _ctx.restore()
-        }
+    getSettings() {
+        return this.settings
+    }
+
+    setX(x) {
+        this.x = x
+    }
+
+    setY(y) {
+        this.y = y
+    }
+
+    clearCanvas() {
+        this.ctx.clearRect(0, 0, canvas.width, canvas.height)
+    }
+
+    drawClock() {
+        this.ctx.save()
+        this.ctx.lineWidth = this.settings.getLineWidth()
+        if (this.settings.areDashesEnabled()) this.drawDashes()
+        if (this.settings.areNumbersEnabled()) this.drawNumbers()
+        this.drawPointers()
+        this.drawDot()
+        this.drawArc()
+        this.ctx.restore()
     }
 
 }
 
-let ClockBuilder = () => {
-    let _canvas
-    let _x
-    let _y
-    let _mode = ClockMode.JUMPING
-    let _settings = ClockSettings()  // enables the builder with some default settings
+class ClockBuilder {
+    constructor() {
+        this.canvas = null
+        this.x = 0
+        this.y = 0
+        this.mode = ClockMode.JUMPING
+        this.settings = new ClockSettings()    // enables the builder with some default settings
 
-    // enables the builder with a default time function
-    let _timeFunction = () => {
-        const today = new Date()
-        return {
-            hours: today.getHours(),
-            minutes: today.getMinutes(),
-            seconds: today.getSeconds(),
-            milliseconds: today.getMilliseconds()
+        // enables the builder with a default time function
+        this.timeFunction = () => {
+            const today = new Date()
+            return {
+                hours: today.getHours(),
+                minutes: today.getMinutes(),
+                seconds: today.getSeconds(),
+                milliseconds: today.getMilliseconds()
+            }
         }
     }
 
-    return {
-        getSettings: () => _settings,
-        setCanvas: (canvas) => {
-            _canvas = canvas
-            return this
-        },
-        setX: (x) => {
-            _x = x
-            return this
-        },
-        setY: (y) => {
-            _y = y
-            return this
-        },
-        setMode: (mode) => {
-            _mode = mode
-            return this
-        },
-        setSettings: (settings) => {
-            _settings = settings
-            return this
-        },
-        setTimeFunction: (timeFunction) => {
-            _timeFunction = timeFunction
-            return this
-        },
-        build: () => {
-            return Clock(_canvas, _x, _y, _mode, _settings, _timeFunction)
-        }
+    setCanvas(canvas) {
+        this.canvas = canvas
+        return this
+    }
+
+    setX(x) {
+        this.x = x
+        return this
+    }
+
+    setY(y) {
+        this.y = y
+        return this
+    }
+
+    setMode(mode) {
+        this.mode = mode
+        return this
+    }
+
+    setSettings(settings) {
+        this.settings = settings
+        return this
+    }
+
+    setTimeFunction(timeFunction) {
+        this.timeFunction = timeFunction
+        return this
+    }
+
+    build() {
+        return new Clock(this.canvas, this.x, this.y, this.mode, this.settings, this.timeFunction)
     }
 }
 
-export { ClockMode, Clock, ClockSettings, ClockBuilder }
+
+export { ClockMode, ClockSettings, Clock, ClockBuilder }
